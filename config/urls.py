0dbc762ceb80818dcef405340dfae44e08fda25e
김나영 <nykim1016@sookmyung.ac.kr>
2022-07-31 13:44:01 +0900

    기본 틀

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from nuribom import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('nuribom/', include('nuribom.urls')),
 ]
